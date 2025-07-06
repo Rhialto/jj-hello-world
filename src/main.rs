696ea0baec46910eec986660e5848d74bbd1136c
Olaf 'Rhialto' Seibert <rhialto@falu.nl>
2025-07-06 16:53:38 +0200

    add a new function

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 ///
 /// This is the best implementation of this program to ever exist.
 
-/// add documentation for main
+/// add documentation for main...
 fn main() {
     print("Hello, world!");
+    print("Goodbye, world!");
 }
 
 // a function that prints a message
